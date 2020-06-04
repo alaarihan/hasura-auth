cba2b5bc9c1682d576ac176f19b3a9a7cc00f513
Alaa Rihan <alaa.addeen@gmail.com>
2020-06-04 07:49:21 +0300

    Init login and refreshToken functions

---
+++ api/login.js
@@ -0,0 +1,37 @@
+const { compare } = require("bcrypt");
+const { getUser, loginUserResponse } = "./common";
+
+const login = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(403).send('Forbidden!');
+  }
+  const { email, password } = req.body.input;
+
+  const user = await getUser({ email: { _eq: email } });
+  if (!user) {
+    throw res
+      .status(401)
+      .send({ message: `No account found for this email: ${email}` });
+  }
+
+  const passwordValid = await compare(password, user.password);
+  if (!passwordValid) {
+    throw res.status(401).send({ message: "Invalid email or password!" });
+  }
+
+  const userToken = loginUserResponse(user);
+
+  if (userToken && userToken.refresh_token) {
+    res.cookie("refresh_token", userToken.refresh_token, {
+      httpOnly: true,
+      maxAge: 43200 * 60 * 1000,
+    });
+  }
+  // success
+  return res.json({
+    jwt_token: userToken.jwt_token,
+    jwt_expires_in: userToken.jwt_expires_in,
+  });
+};
+
+module.exports = login;
---
+++ api/refresh.js
@@ -0,0 +1,35 @@
+const { verify } = require("jsonwebtoken");
+const cookie = require('cookie')
+const refreshToken = async (req, res) => {
+  if(!req.headers.cookie){
+    throw res.status(401).json({ message: "You have to login!" });
+  }
+  const cookies = cookie.parse(req.headers.cookie)
+  const refresh_token = cookies.refresh_token;
+  if (!refresh_token) {
+    throw res.status(401).json({ message: "You have to login!" });
+  }
+
+  let refreshTokenData;
+  try {
+    refreshTokenData = verify(refresh_token, process.env.JWT_KEY);
+  } catch (e) {
+    throw res.status(401).send({ message: `Invalid token!` });
+  }
+
+  if (!refreshTokenData || !refreshTokenData.userId) {
+    throw res.status(401).send({ message: `Invalid refresh token!` });
+  }
+  const user = await getUser({ id: { _eq: userId } });
+  if (!user) {
+    throw res.status(401).send({ message: `The refresh token user is not valid anymore!` });
+  }
+  const userToken = loginUserResponse(user);
+
+  return res.json({
+    jwt_token: userToken.jwt_token,
+    jwt_expires_in: userToken.jwt_expires_in,
+  });
+};
+
+module.exports = refreshToken;
