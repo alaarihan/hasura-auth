d14523469d34ed79dc703b3b7ece4d6a886e9e12
Alaa Rihan <alaa.addeen@gmail.com>
2020-06-04 08:48:37 +0300

    try to fix jwt for hasura

--- api/lib/common.js
+++ api/lib/common.js
@@ -29,7 +29,7 @@ const loginUserResponse = (user) => {
         "https://hasura.io/jwt/claims": {
           "x-hasura-allowed-roles": [user.role],
           "x-hasura-default-role": user.role,
-          "x-hasura-user-id": user.id,
+          "x-hasura-user-id": user.id.toString(),
         },
       },
       process.env.JWT_KEY,
