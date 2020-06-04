18a644cd7716750c5ca33d90458395fbc8788a4d
Alaa Rihan <alaa.addeen@gmail.com>
2020-06-04 09:44:47 +0300

    Don't thow response just send it

--- api/refresh.js
+++ api/refresh.js
@@ -3,27 +3,27 @@ const cookie = require('cookie')
 const { getUser, loginUserResponse } = require("./lib/common");
 const refreshToken = async (req, res) => {
   if(!req.headers || !req.headers.cookie){
-    throw res.status(401).json({ message: "You have to login!" });
+    res.status(401).json({ message: "You have to login!" });
   }
   const cookies = cookie.parse(req.headers.cookie)
   const refresh_token = cookies.refresh_token;
   if (!refresh_token) {
-    throw res.status(401).json({ message: "You have to login!" });
+    res.status(401).json({ message: "You have to login!" });
   }
 
   let refreshTokenData;
   try {
     refreshTokenData = verify(refresh_token, process.env.JWT_KEY);
   } catch (e) {
-    throw res.status(401).send({ message: `Invalid token!` });
+    res.status(401).jsom({ message: `Invalid token!` });
   }
 
   if (!refreshTokenData || !refreshTokenData.userId) {
-    throw res.status(401).send({ message: `Invalid refresh token!` });
+    res.status(401).json({ message: `Invalid refresh token!` });
   }
   const user = await getUser({ id: { _eq: refreshTokenData.userId } });
   if (!user) {
-    throw res.status(401).send({ message: `The refresh token user is not valid anymore!` });
+    res.status(401).send({ message: `The refresh token user is not valid anymore!` });
   }
   const userToken = loginUserResponse(user);
 
